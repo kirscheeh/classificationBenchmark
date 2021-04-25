4b27b5a1567200bd70d1d3b20e9a835d55e0e56d
kirscheeh <kirsten.goebel@gmx.de>
2021-04-25 15:34:55 +0200

    changes

--- scripts/changeHeaderCCMetagen.py
+++ scripts/changeHeaderCCMetagen.py
@@ -32,11 +32,11 @@ with open(nt_in) as nt:
     for line in nt:
         if line.startswith(">"):
             splited_rec = re.split (r'(>| )', line)
-            print(splited_rec)
+#            print(splited_rec)
             accession = splited_rec[2]
-            print(accession)
+#            print(accession)
             taxid = get_tax_id_dic(accession,acc2tax_dic)
-            print(taxid)
+#            print(taxid)
             line = ">" + taxid + "|" + "".join(splited_rec[2:])
         new_nt.write(line)
             
